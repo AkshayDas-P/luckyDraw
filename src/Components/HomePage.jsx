2b82777469c91006f03a45fd57e699bf3e4ad738
AkshayDas-P <88092620+AkshayDas-P@users.noreply.github.com>
2024-03-09 22:11:16 +0530

    Add files via upload

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,44 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import './HomePage.css'; 
+import './HomePage.css';
 import congrats from './Images/congrats.png';
 import spinButton from './Images/spinButton.png';
 
 const HomePage = () => {
-  const [count1, setCount1] = useState(1);
-  const [count2, setCount2] = useState(2);
-  const [count3, setCount3] = useState(3);
-  const [count4, setCount4] = useState(4);
-  const [count5, setCount5] = useState(5);
-  const [count6, setCount6] = useState(6);
-
-  const [isGenerating, setIsGenerating] = useState(false);
-  const [showPrizeText, setShowPrizeText] = useState(false);
+  const [counts, setCounts] = useState([1, 2, 3, 4, 5, 6]);
+  const [isGenerating, setIsGenerating] = useState(false); // initial generating value as false at first
+  const [showPrizeText, setShowPrizeText] = useState(false); // 1st Prize text given false at initial stage
 
   useEffect(() => {
     let interval;
-
+    //setting interval for counter function
     if (isGenerating) {
       interval = setInterval(() => {
-        setCount1(generateRandom());
-        setCount2(generateRandom());
-        setCount3(generateRandom());
-        setCount4(generateRandom());
-        setCount5(generateRandom());
-        setCount6(generateRandom());
+        setCounts(counts.map(generateRandom));
       }, 500);
     }
 
     return () => {
       clearInterval(interval);
     };
-  }, [isGenerating]);
+  }, [isGenerating, counts]); //dependency array as generating and counts
 
-  const generateRandom = (maxLimit = 10) => {
+  //for getting random result inside 1 - 10, calling the following function
+  const generateRandom = (count) => {
+    return generateRandomCount();
+  };
+
+  const generateRandomCount = (maxLimit = 10) => {
     let rand = Math.random() * maxLimit;
     rand = Math.floor(rand);
     return rand;
   };
-
+  // after initializing first function and after specific timeout result with the text is shown after the folowing "getWinner" function
   const getWinner = () => {
     setIsGenerating(true);
 
@@ -51,14 +45,13 @@ const HomePage = () => {
   const countBoxStyle = {
     display: 'flex',
     justifyContent: 'space-between',
-    marginTop: '20px', 
+    marginTop: '50px',
   };
 
   const countStyle = {
-    flex: '1',
     textAlign: 'center',
-    border: '1px solid #9950b9', 
-    padding: '10px 15px', 
+    border: '1px solid #9950b9',
+    padding: '10px 15px',
     color: '#ffffff',
     fontSize: '40px',
     fontWeight: '800',
@@ -70,32 +63,33 @@ const HomePage = () => {
     position: 'relative',
   };
 
-
   const prizeTextStyle = {
-    fontSize: '24px',
+    fontSize: '40px',
     fontWeight: 'bold',
     color: 'white',
     fontStyle: 'italic',
     animation: showPrizeText ? 'flashText 1s infinite' : 'none',
-}
-
+    position: 'absolute',
+    top: '-60px',
+    left: '50%',
+    transform: 'translate(-50%, 0%)',
+    transition: '.5s',
+  }
   return (
     <div className="main-container" style={backgroundImageStyle}>
-      <img src={congrats} alt="img" style={{'width':'80%'}} />
-      {showPrizeText && <div style={prizeTextStyle}>1ST PRIZE</div>}
+      <img src={congrats} alt="img" style={{ 'width': '80%' }} />
       <div id="countBox" className="count-box" style={countBoxStyle}>
-        <div className="count" style={countStyle}>{count1}</div>
-        <div className="count" style={countStyle}>{count2}</div>
-        <div className="count" style={countStyle}>{count3}</div>
-        <div className="count" style={countStyle}>{count4}</div>
-        <div className="count" style={countStyle}>{count5}</div>
-        <div className="count" style={countStyle}>{count6}</div>
+        {showPrizeText && <div style={prizeTextStyle}>1ST PRIZE</div>}
+        {counts.map((count, index) => (
+          <div key={index} className="count" style={countStyle}>
+            {count}
+          </div>
+        ))}
       </div>
       <div className="btn-winner" onClick={getWinner}>
-        <img src={spinButton} alt="Spin Button" style={{'width':'200px'}} />
+        <img src={spinButton} alt="Spin Button" style={{ 'width': '300px' }} />
       </div>
     </div>
   );
 };
-
 export default HomePage;
